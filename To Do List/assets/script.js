4d26a42ce2df5eb10c50f2d24a0af2503787e2c4
Ana Burcovschi <ana@burcovschi.com>
2025-01-20 17:48:27 +0200

    overview

--- To Do List/assets/script.js
+++ To Do List/assets/script.js
@@ -1,34 +1,46 @@
+// jQuery code to manage the To-Do List functionality
 $(document).ready(function () {
-    let message;
-    $("#new-list").on("keypress", function(e){
-        message = $(this).val();
-        if (message) {            
-            if (e.key === "Enter") {
-            $(".list-container").append(`
-                <div class="list">
-                    <p>${message}</p>
-                    <img src="./assets/img/check.svg" class="check">
-                     <img src="./assets/img/trash.svg" alt="" class="delete">
-                </div>
+    let message; // Variable to hold the message from the input
+
+    // Event listener for keypress in the input field
+    $("#new-list").on("keypress", function(e) {
+        message = $(this).val(); // Get the value from the input
+        if (message) { // Check if there is a message
+            if (e.key === "Enter") { // Check if the Enter key is pressed
+                // Append a new list item to the list container
+                $(".list-container").append(`
+                    <div class="list">
+                        <p>${message}</p> <!-- Display the message -->
+                        <img src="./assets/img/check.svg" class="check" alt="Check"> <!-- Check icon -->
+                        <img src="./assets/img/trash.svg" alt="Delete" class="delete"> <!-- Delete icon -->
+                    </div>
                 `);
-            $(this).val("");
+                $(this).val(""); // Clear the input field
             }
         }
         
+        // Attach click event handlers for delete and check icons
         $(".delete").on("click", deleteItem);
         $(".check").on("click", check);
     });
 
+    // Initial event handlers for existing delete and check icons
     $(".delete").on("click", deleteItem);
     $(".check").on("click", check);
+
+    // Function to delete an item
     function deleteItem() {
         $(this).parent().hide(2000, function() {
-            $(this).remove(); 
+            $(this).remove(); // Remove the item from the DOM after hiding
         });
     }
-    function check(){
-        $(this).parent().toggleClass("checked");
-        $(this).prev().toggleClass("checked-p")
+
+    // Function to check an item
+    function check() {
+        $(this).parent().toggleClass("checked"); // Toggle checked class on the parent
+        $(this).prev().toggleClass("checked-p"); // Toggle strikethrough on the paragraph
     }
-    $(".list-container").sortable()
+
+    // Make the list container sortable
+    $(".list-container").sortable();
 });

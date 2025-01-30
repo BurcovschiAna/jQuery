9ea781659661fdf372bece8d54bf3590944b1d66
Ana Burcovschi <ana@burcovschi.com>
2025-01-30 21:02:39 +0200

    pagination

---
+++ Movie website/assets/js/data.js
@@ -0,0 +1,415 @@
+let allMovies = [
+    {
+        movieName: "A Star is Born",
+        moviePoster: "./assets/img/movie/A star is born movie.jpg",
+        movieRating: "5",
+        movieIndex: "0",
+        movieCategory: ["Drama"]
+    },
+    {
+        movieName: "Avatar",
+        moviePoster: "./assets/img/movie/Avatar movie.jpg",
+        movieRating: "5",
+        movieIndex: "1",
+        movieCategory: ["SF"]
+    },
+    {
+        movieName: "Avengers Infinity War",
+        moviePoster: "./assets/img/movie/Avengers infiny war movie.jpg",
+        movieRating: "4.9",
+        movieIndex: "2",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Alligiant",
+        moviePoster: "./assets/img/movie/Alligiant.jpg",
+        movieRating: "2",
+        movieIndex: "3",
+        movieCategory: ["SF"]
+    },
+    {
+        movieName: "Avengers",
+        moviePoster: "./assets/img/movie/Avengers movie.jpg",
+        movieRating: "4.5",
+        movieIndex: "4",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Damsel",
+        moviePoster: "./assets/img/movie/Damsel movie.jpg",
+        movieRating: "2.5",
+        movieIndex: "5",
+        movieCategory: ["SF", "Drama"]
+    },
+    {
+        movieName: "Divergent",
+        moviePoster: "./assets/img/movie/Divergent movie.jpg",
+        movieRating: "3.5",
+        movieIndex: "6",
+        movieCategory: ["Drama"]
+    },
+    {
+        movieName: "Ferdinant",
+        moviePoster: "./assets/img/movie/Ferdinant movie.jpg",
+        movieRating: "4",
+        movieIndex: "7",
+        movieCategory: ["Kids & Family"]
+    },
+    {
+        movieName: "Grinch",
+        moviePoster: "./assets/img/movie/Grinch movie.jpg",
+        movieRating: "4.5",
+        movieIndex: "8",
+        movieCategory: ["Kids & Family"]
+    },
+    {
+        movieName: "Grown ups 2",
+        moviePoster: "./assets/img/movie/Grown ups 2 movie.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["Comedies"]
+    },
+    {
+        movieName: "Grown ups",
+        moviePoster: "./assets/img/movie/Grown ups movie.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["Comedies"]
+    },
+
+    {
+        movieName: "Hobbit the Battle of the Five Armies",
+        moviePoster: "./assets/img/movie/Hobbit the battle of the five armies.jpg",
+        movieRating: "3",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Hobbit the Disolation of Smaug",
+        moviePoster: "./assets/img/movie/Hobbit the disolation of smaug.jpg",
+        movieRating: "3.5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Hobbit",
+        moviePoster: "./assets/img/movie/Hobbit.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Hotel Transylvania",
+        moviePoster: "./assets/img/movie/Hotel transylvania 1 movie.jpg",
+        movieRating: "4.5",
+        movieIndex: "",
+        movieCategory: ["Kids & Family"]
+    },
+    {
+        movieName: "Hotel Transylvania 2",
+        moviePoster: "./assets/img/movie/Hotel transylvania 2 movie.jpg",
+        movieRating: "4.5",
+        movieIndex: "",
+        movieCategory: ["Kids & Family"]
+    },
+    
+    {
+        movieName: "Hotel Transylvania a Monster Vacation",
+        moviePoster: "./assets/img/movie/Hotel transylvania 3 movie.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["Kids & Family"]
+    },
+    
+    {
+        movieName: "Hotel Transylvania Transformania",
+        moviePoster: "./assets/img/movie/Hotel transylvania 4 movie.jpg",
+        movieRating: "4.5",
+        movieIndex: "",
+        movieCategory: ["Kids & Family"]
+    },
+    
+
+    {
+        movieName: "In the Tall Grass",
+        moviePoster: "./assets/img/movie/In the tall grass movie.jpg",
+        movieRating: "3",
+        movieIndex: "",
+        movieCategory: ["Horror"]
+    },
+    
+    {
+        movieName: "Inception",
+        moviePoster: "./assets/img/movie/inception movie.jpg",
+        movieRating: "5",
+        movieIndex: "",
+        movieCategory: ["Action", "Drama", "SF"]
+    },
+    
+    {
+        movieName: "Insurgent",
+        moviePoster: "./assets/img/movie/Insurgent movie.jpg",
+        movieRating: "3",
+        movieIndex: "",
+        movieCategory: ["SF", "Drama"]
+    },
+    
+    {
+        movieName: "Iron Man 2",
+        moviePoster: "./assets/img/movie/Iron man 2 movie.jpg",
+        movieRating: "5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    
+    {
+        movieName: "Iron Man 3",
+        moviePoster: "./assets/img/movie/Iron man 3 movie.webp",
+        movieRating: "5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    
+    {
+        movieName: "Iron Man",
+        moviePoster: "./assets/img/movie/Iron man movie.jpg",
+        movieRating: "5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    
+    {
+        movieName: "IT",
+        moviePoster: "./assets/img/movie/IT movie.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["Horror"]
+    },
+    
+    {
+        movieName: "John Wick 2",
+        moviePoster: "./assets/img/movie/John Wivk 2 movie.jpg",
+        movieRating: "2",
+        movieIndex: "",
+        movieCategory: ["Action"]
+    },
+    {
+        movieName: "John Wick",
+        moviePoster: "./assets/img/movie/John Wivk movie.jpg",
+        movieRating: "2",
+        movieIndex: "",
+        movieCategory: ["Action"]
+    },
+    {
+        movieName: "Leo",
+        moviePoster: "./assets/img/movie/Leo movie'.jpg",
+        movieRating: "4.5",
+        movieIndex: "",
+        movieCategory: ["Kids & Family"]
+    },
+    {
+        movieName: "Liar Liar",
+        moviePoster: "./assets/img/movie/Liar liar movie.webp",
+        movieRating: "2",
+        movieIndex: "",
+        movieCategory: ["comedies"]
+    },
+    {
+        movieName: "Little Women",
+        moviePoster: "./assets/img/movie/Little women movie.jpg",
+        movieRating: "3",
+        movieIndex: "",
+        movieCategory:["Drama"]
+    },
+    {
+        movieName: "Lord of the Rings",
+        moviePoster: "./assets/img/movie/Lord of the rings 1 movie.jpg",
+        movieRating: "3",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Lord of the Rings The Return on the King",
+        moviePoster: "./assets/img/movie/Lord of the rings The return on the king movie.jpg",
+        movieRating: "3.5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Lord of the Rings the Two Towers",
+        moviePoster: "./assets/img/movie/Lord of the rings the two towers movie.jpg",
+        movieRating: "3",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Man of Steel",
+        moviePoster: "./assets/img/movie/Man of steel.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Matrix 2 ",
+        moviePoster: "./assets/img/movie/Matrix 2 movie.jpg",
+        movieRating: "3.5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Matrix Reloaded",
+        moviePoster: "./assets/img/movie/Matrix reloaded movie.jpg",
+        movieRating: "3.5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Meg 2",
+        moviePoster: "./assets/img/movie/Meg 2 movie.jpg",
+        movieRating: "3.5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Mr Bean Holiday",
+        moviePoster: "./assets/img/movie/Mr Bean Holiday movie.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["Kids & Family", "Comedies"]
+    },
+    {
+        movieName: "Mummy",
+        moviePoster: "./assets/img/movie/Mymmt movie.webp",
+        movieRating: "3",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Orion and the Dark",
+        moviePoster: "./assets/img/movie/Orion and the dark movie.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["Kids & Family"]
+    },
+    {
+        movieName: "Red Notice",
+        moviePoster: "./assets/img/movie/Red notice movie.jpg",
+        movieRating: "2.5",
+        movieIndex: "",
+        movieCategory: ["Drama", "Action"]
+    },
+    {
+        movieName: "Red One",
+        moviePoster: "./assets/img/movie/Red One movie.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["SF", "Action", "Comedies"]
+    },
+    {
+        movieName: "Split",
+        moviePoster: "./assets/img/movie/Split movie.jpg",
+        movieRating: "4.5",
+        movieIndex: "",
+        movieCategory: ["Horror"]
+    },
+    {
+        movieName: "The Dark Night",
+        moviePoster: "./assets/img/movie/The dark night.jpg",
+        movieRating: "4.5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "The Lorax",
+        moviePoster: "./assets/img/movie/The lorax movie.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["Kids & Family"]
+    },
+    {
+        movieName: "The Nun",
+        moviePoster: "./assets/img/movie/The nun movie.jpg",
+        movieRating: "3.5",
+        movieIndex: "",
+        movieCategory: ["Horror"]
+    },
+    {
+        movieName: "The Purge",
+        moviePoster: "./assets/img/movie/The purge movie.jpg",
+        movieRating: "3",
+        movieIndex: "",
+        movieCategory: ["Horror"]
+    },
+    {
+        movieName: "The Sea Best",
+        moviePoster: "./assets/img/movie/The sea best movie.jpg",
+        movieRating: "4.5",
+        movieIndex: "",
+        movieCategory: ["Kids & Family"]
+    },
+    {
+        movieName: "The Shallows",
+        moviePoster: "./assets/img/movie/The shallows movie.jpg",
+        movieRating: "3.5",
+        movieIndex: "",
+        movieCategory: ["Horror"]
+    },
+    {
+        movieName: "Thor",
+        moviePoster: "./assets/img/movie/Thor movie.jpg",
+        movieRating: "4.5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Thor Ragnarok",
+        moviePoster: "./assets/img/movie/Thor ragnarok movie.jpg",
+        movieRating: "4.5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Thor the Dark World",
+        moviePoster: "./assets/img/movie/Thor the dark world movie.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Venom Let There Be Carnage",
+        moviePoster: "./assets/img/movie/Venom let there be carnage.jpg",
+        movieRating: "3.5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Venom",
+        moviePoster: "./assets/img/movie/Venom.webp",
+        movieRating: "4.5",
+        movieIndex: "",
+        movieCategory: ["SF", "Action"]
+    },
+    {
+        movieName: "Wanted",
+        moviePoster: "./assets/img/movie/Wanted movie.jpg",
+        movieRating: "2.5",
+        movieIndex: "",
+        movieCategory: ["Action"]
+    },
+    
+    {
+        movieName: "Wonder",
+        moviePoster: "./assets/img/movie/Wonder movie.jpg",
+        movieRating: "5",
+        movieIndex: "",
+        movieCategory: ["Kids & Family", "Drama"]
+    },
+    {
+        movieName: "Zohan",
+        moviePoster: "./assets/img/movie/Zohan movie.jpg",
+        movieRating: "4",
+        movieIndex: "",
+        movieCategory: ["Comedies"]
+    },
+    
+]

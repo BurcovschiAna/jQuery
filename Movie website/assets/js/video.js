8cc3268a8db5e26796384ff251739d1a14605a71
Ana Burcovschi <ana@burcovschi.com>
2025-02-03 15:32:58 +0200

    video player

--- Movie website/assets/js/video.js
+++ Movie website/assets/js/video.js
@@ -1,87 +1,150 @@
 $(document).ready(function () {
-    const video = $("#main-video")[0];
-    //? Play video
-    function playPause(){
-        if(video.paused){
-            play()
-        } else{
-            pause()
+    const video = $("#main-video");
+
+    function playPause() {
+        if (video[0].paused) {
+            play();
+        } else {
+            pause();
         }
     }
+
     function play() {
         $(".play").html("pause");
-        video.play();
+        video[0].play();
     }
-    function pause(){
+
+    function pause() {
         $(".play").html("play_arrow");
-        video.pause();
+        video[0].pause();
     }
-    $(video).on("play", function() {
-        $(".play").html("Pause");
+
+    $(video).on("play", function () {
+        $(".play").html("pause");
     });
 
-    $(video).on("pause", function() {
+
+    $(video).on("pause", function () {
         $(".play").html("play_arrow");
     });
+
+    $(video).on("click", function () {
+        $(".play").html("pause");
+        play()
+    });
+
     $(".play").on("click", playPause);
 
-    // ! forward
     $(".forward").on("click", function () {
-        video.currentTime += 10;
-    });
-    // ! rewind
-    $(".replay").on("click", function () {
-        video.currentTime -= 10;
+        video[0].currentTime += 10;
     });
 
-    // ! Duration
+    $(".replay").on("click", function () {
+        video[0].currentTime -= 10;
+    });
+
     $(video).on("loadeddata", function (e) {
         let duration = e.target.duration;
         let totalMin = Math.floor(duration / 60);
         let totalSec = Math.floor(duration % 60);
-        totalSec < 10 ? totalSec = "0" + totalSec : totalSec;
+        totalSec = totalSec < 10 ? "0" + totalSec : totalSec;
         $(".duration").html(`${totalMin}:${totalSec}`);
     });
-    // ! Current time
+
     $(video).on("timeupdate", function (e) {
         let currentTime = e.target.currentTime;
         let currentMin = Math.floor(currentTime / 60);
         let currentSec = Math.floor(currentTime % 60);
-        currentSec < 10 ? currentSec = "0" + currentSec : currentSec;
+        currentSec = currentSec < 10 ? "0" + currentSec : currentSec;
         $(".current-time").html(`${currentMin}:${currentSec}`);
         let duration = e.target.duration;
         let progres = (currentTime / duration) * 100;
         $(".progress-bar").css("width", `${progres}%`);
     });
 
-    // ! progress bar
     $(".progress").on("click", function (e) {
-        let duration = video.duration;
-        console.log(duration);
-        let progress = $(".progress")[0].clientWidth;
+        let duration = video[0].duration;
+        let progress = $(this).width();
         let offset = e.offsetX;
-        video.currentTime = (offset / progress) * duration 
+        video[0].currentTime = (offset / progress) * duration;
     });
-    // ! volume
+
+    $(".progress").on("mousemove", function (e) {
+        let progresWidth = $(this).width();
+        let x = e.offsetX;
+        let duration = video[0].duration;
+        let progres = Math.floor((x / progresWidth) * duration);
+        let currentMin = Math.floor(progres / 60);
+        let currentSec = Math.floor(progres % 60);
+        currentSec = currentSec < 10 ? "0" + currentSec : currentSec;
+        $('.progres-time').css('--x', `${x}px`);
+        $(".progres-time").css("display", "block");
+        $(".progres-time").html(`${currentMin}:${currentSec}`);
+    });
+
+    $(".progress").on("mouseleave", function () {
+        $(".progres-time").css("display", "none");
+    });
+
     $("#volume").on("change", function () {
-        video.volume = $(this).val() / 100;
-        if($(this).val() == 0){
+        video[0].volume = $(this).val() / 100;
+        if ($(this).val() == 0) {
             $(".volume").html("volume_off");
-        } else if($(this).val() < 40){
+        } else if ($(this).val() < 40) {
             $(".volume").html("volume_down");
         } else {
             $(".volume").html("volume_up");
-        } 
+        }
     });
+
     $(".volume").on("click", function () {
-        if($("#volume").val() == 0){
-            video.volume = 0.8
+        if ($("#volume").val() == 0) {
+            video[0].volume = 0.8;
             $("#volume").val(80);
             $(".volume").html("volume_up");
-        } else{
-            video.volume = 0
+        } else {
+            video[0].volume = 0;
             $("#volume").val(0);
             $(".volume").html("volume_off");
         }
+    });
+
+    $("#settings").on("click", function () {
+        $(".settings").fadeToggle(500);
+    });
+
+    $("#picture-in-picture").on("click", function () {
+        video[0].requestPictureInPicture();
+    });
+
+    $("#fullscreen").on("click", function () {
+        if (video.hasClass("openFullScreen")) {
+            video.removeClass("openFullScreen");
+            $(".controls-container").addClass("");
+            document.exitFullscreen();
+            $(this).text("fullscreen");
+        } else {
+            video.addClass("openFullScreen");
+            video[0].requestFullscreen();
+            $(this).text("fullscreen_exit");
+        }
+    });
+    
+    $(".playback div").on("click", function () {
+        $(".playback div").each(function () {
+            $(this).removeClass("active");
+        });
+        $(this).addClass("active");
+        let speed = $(this).attr("data-speed");
+        video[0].playbackRate = speed;
+    });
+    $(video).on("mouseover", function () {
+        $(".controls-container").slideDown();
+    });
+    $(video).on("mouseleave", function () {
+        $(".controls-container").slideUp();
+    });
+    $(".controls-container").on("mouseover", function () {
+        $(this).css("display", "block");
     });
 });
